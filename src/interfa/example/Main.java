18cfe4979f64aa9d4fa377f277395b285c59d4ea
Anil1911 <anilguthula3@gmail.com>
2024-04-12 17:03:27 +0530

    ok

--- src/interfa/example/Main.java
+++ src/interfa/example/Main.java
@@ -4,7 +4,11 @@ public class Main {
     public static void main(String[] args) {
         SBI s=new SBI();
         s.rateOfInterest();
+        FBI f=new FBI();
+        f.rateOfInterest();
         System.out.println("Rate of interest"+s.rateOfInterest());
+
+        System.out.println("Rate of interest"+f.rateOfInterest());
 
     }
 }
